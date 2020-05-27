b1d92a39392486c6eb1c2e9a2b486d4ea90b6049
Revan/Irvan/Erpan <47099428+IrvanPrama@users.noreply.github.com>
2020-05-27 21:52:17 +0800

    crud php isi variasi
    ditambah sedikit variasi biar gak teralu sama dengan teman teman

---
+++ php/php crud/tambah.php
@@ -0,0 +1,78 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION["login"])) {
+    header('Location: login.php');
+    exit;
+}
+
+require 'functions.php';
+// cek apakah tombol submit sudah ditekan atau blm
+if (isset($_POST["submit"])) {
+
+    // var_dump($_POST);
+    // cek data sudah ditambahkan atau tidak
+    if (tambah($_POST) > 0) {
+        echo "
+        <script>
+            alert('Data BERHASIL ditambahkan');
+            document.location.href = 'index.php';
+        </script>
+        ";
+    } else {
+        echo "
+        <script>
+            alert('Data GAGAL ditambahkan!');
+            document.location.href = 'index.php';
+        </script>
+        ";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tambah Data Produk</title>
+</head>
+
+<body>
+    <h1>Tambah Data Produk</h1>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <ul>
+            <li>
+                <label for="nrp">NRP:</label>
+                <input type="text" name="nrp" id="nrp">
+            </li>
+            <li>
+                <label for="nama">Nama:</label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="harga">Harga:</label>
+                <input type="text" name="Hhrga" id="harga">
+            </li>
+            <li>
+                <label for="pemilik">Pemilik:</label>
+                <input type="text" name="pemilik" id="pemilik">
+            </li>
+            <li>
+                <label for="alamat">Alamat:</label>
+                <input type="text" name="alamat" id="alamat">
+            </li>
+            <li>
+                <label for="gambar">Gambar:</label>
+                <input type="text" name="gambar" id="gambar">
+            </li>
+            <button type="submit" name="submit">Tambah</button>
+        </ul>
+    </form>
+</body>
+
+</html>
---
+++ php/php crud/ubah.php
@@ -0,0 +1,85 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION["login"])) {
+    header('Location: login.php');
+    exit;
+}
+
+require 'functions.php';
+
+// Ambil data dari url
+$id = $_GET["id"];
+
+// query data komoditas berdasarkan id
+$commodity = query("SELECT * FROM komoditas WHERE id = $id")[0];
+
+
+// cek apakah tombol submit sudah ditekan atau blm
+if (isset($_POST["submit"])) {
+
+    // cek data sudah diubah atau tidak
+    if (ubah($_POST) > 0) {
+        echo "
+        <script>
+            alert('Data BERHASIL diubah');
+            document.location.href = 'index.php';
+        </script>
+        ";
+    } else {
+        echo "
+        <script>
+            alert('Data GAGAL diubah!');
+            document.location.href = 'index.php';
+        </script>
+        ";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ubah Data</title>
+</head>
+
+<body>
+    <h1>Ubah Data Produk</h1>
+
+    <form action="" method="post">
+        <input type="hidden" name="id" value="<?= $commodity["id"]; ?>">
+        <ul>
+            <li>
+                <label for="nrp">NRP:</label>
+                <input type="text" name="nrp" id="nrp" required value="<?= $commodity["nrp"]; ?>">
+            </li>
+            <li>
+                <label for="nama">Nama:</label>
+                <input type="text" name="nama" id="nama" required value="<?= $commodity["nama"]; ?>">
+            </li>
+            <li>
+                <label for="harga">Harga:</label>
+                <input type="text" name="harga" id="harga" required value="<?= $commodity["harga"]; ?>">
+            </li>
+            <li>
+                <label for="pemilik">Pemilik:</label>
+                <input type="text" name="pemilik" id="pemilik" required value="<?= $commodity["pemilik"]; ?>">
+            </li>
+            <li>
+                <label for="alamat">Alamat:</label>
+                <input type="text" name="alamat" id="alamat" required value="<?= $commodity["alamat"]; ?>">
+            </li>
+            <li>
+                <label for="gambar">Gambar:</label>
+                <input type="text" name="gambar" id="gambar" required value="<?= $commodity["gambar"]; ?>">
+            </li>
+            <button type="submit" name="submit">Ubah</button>
+        </ul>
+    </form>
+</body>
+
+</html>
